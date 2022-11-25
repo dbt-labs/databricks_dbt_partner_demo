92c26f76dcdc370c70cc667b7600423fe666cd22
Stephan Durry <stephan.durry@fishtownanalytics.com>
2022-11-25 11:56:57 +0000

    demo-prep

--- models/marts/intermediate/stg_tpch_orders__describe.py
+++ models/marts/intermediate/stg_tpch_orders__describe.py
@@ -2,7 +2,7 @@ def model(dbt, session):
     dbt.config(
         materialized="table",
         create_notebook=False, # True writes to /Shared/dbt_python_model/
-        cluster_id="0408-145556-egmplhh0"
+        cluster_id="0830-163228-q22s4sv5"
     )
 
     orders = dbt.ref("stg_tpch_orders")
